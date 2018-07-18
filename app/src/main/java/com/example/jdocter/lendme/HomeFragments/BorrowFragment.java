80f48a19b9240aa28d7e8a8d194d067073abf578
Benjamin Rodriguez <benj1man3@fb.com>
2018-07-18 11:49:13 -0700

    Added icons for bottom nav view and side bar

--- app/src/main/java/com/example/jdocter/lendme/HomeFragments/BorrowFragment.java
+++ app/src/main/java/com/example/jdocter/lendme/HomeFragments/BorrowFragment.java
@@ -10,14 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.jdocter.lendme.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BorrowFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BorrowFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BorrowFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
