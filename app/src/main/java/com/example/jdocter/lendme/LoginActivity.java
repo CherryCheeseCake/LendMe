9782f039ca84bfacdab036acba400083e4f4a44f
JingyiRose <roliu@g.hmc.edu>
2018-07-25 11:38:10 -0700

    scrollable effect

--- app/src/main/java/com/example/jdocter/lendme/LoginActivity.java
+++ app/src/main/java/com/example/jdocter/lendme/LoginActivity.java
@@ -24,7 +24,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ParseUser.logOut();
         
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
