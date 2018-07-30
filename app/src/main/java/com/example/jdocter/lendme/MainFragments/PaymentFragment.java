97ddb8c814d2fe51f61697f42a7d25d9d949e3c5
JingyiRose <roliu@g.hmc.edu>
2018-07-30 13:16:09 -0700

    add payment fragment

--- app/src/main/java/com/example/jdocter/lendme/MainFragments/PaymentFragment.java
+++ app/src/main/java/com/example/jdocter/lendme/MainFragments/PaymentFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.jdocter.lendme.R;
 
@@ -14,6 +16,15 @@ public class PaymentFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_payment, container, false);
+        View view= inflater.inflate(R.layout.fragment_payment, container, false);
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.card_type, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        return view;
     }
 }
